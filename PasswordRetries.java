0a0e1238d9df477e75c01cbe91302286ba44b44c
Noor ul ain Zahra <zahrabscs006@gmail.com>
2025-09-29 23:57:52 +0500

    Password Retries While Login

---
+++ PasswordRetries.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class PasswordRetries {
+    public static void main(String[] args) {
+       String correctPassword = "secret123";
+        String enteredPassword;
+        int attempts=3;
+
+        Scanner input = new Scanner(System.in);
+
+        while(attempts>0){
+             attempts--;
+            System.out.print("Enter the password:");
+            enteredPassword = input.nextLine();
+
+            if(enteredPassword.equals(correctPassword)){
+                System.out.println("Login Successful!");
+                break;
+            }
+            else{
+                if (attempts > 0) {
+                    System.out.println("Incorrect Password. You have " + attempts + " attempts left.");
+                } else {
+                    System.out.println("Account Blocked. Please Contact support.");  
+                }
+            }
+        }
+        input.close();
+    }
+        
+}
